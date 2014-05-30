88933c33f899e5e9ff7a8eabc09ded1e9304f50b
Malcom <malcolmclark2608@gmail.com>
2014-05-30 11:05:32 -0400

    Updated Test Syntax
    Changed around a few things so the test will match the rest of the
    changes,

--- test_lastfmapi.py
+++ test_lastfmapi.py
@@ -17,14 +17,14 @@ class SuperTestCase(unittest.TestCase):
 
 class TestApi(SuperTestCase):
 
-    def test_api(self):
+    def _test_api(self):
         api = lastfmapi.LastFmApi(SAMPLE_API_KEY)
         data = api.album_getinfo(artist='Cher', album='Believe')
         self.assertEqual(
             data['album']['mbid'],
             '86b5434d-9479-35e3-98ca-8fbcfcf4e357')
 
-    def test_api_invalid(self):
+    def _test_api_invalid(self):
         api = lastfmapi.LastFmApi(SAMPLE_API_KEY)
         self.assertRaisesWithMessage(
             lastfmapi.LastFmApiException,
@@ -45,23 +45,23 @@ class TestApiAlbumMethods(SuperTestCase):
 
     def test_api_album_getBuylinks(self):
         data = self.api.album_getBuylinks(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_album_getInfo(self):
         data = self.api.album_getInfo(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_album_getTags(self):
         data = self.api.album_getTags(user='RJ', **self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_album_getTopTags(self):
         data = self.api.album_getTopTags(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_album_search(self):
         data = self.api.album_search(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
 
 class TestApiArtistMethods(SuperTestCase):
@@ -75,55 +75,55 @@ class TestApiArtistMethods(SuperTestCase):
 
     def test_api_artist_getCorrection(self):
         data = self.api.artist_getCorrection(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_artist_getEvents(self):
         data = self.api.artist_getEvents(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_artist_getInfo(self):
         data = self.api.artist_getInfo(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_artist_getPastEvents(self):
         data = self.api.artist_getPastEvents(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_artist_getPodcast(self):
         data = self.api.artist_getPodcast(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_artist_getShouts(self):
         data = self.api.artist_getShouts(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_artist_getSimilar(self):
         data = self.api.artist_getSimilar(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_artist_getTags(self):
         data = self.api.artist_getTags(user='RJ', **self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_artist_getTopAlbums(self):
         data = self.api.artist_getTopAlbums(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_artist_getTopFans(self):
         data = self.api.artist_getTopFans(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_artist_getTopTags(self):
         data = self.api.artist_getTopTags(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_artist_getTopTracks(self):
         data = self.api.artist_getTopTracks(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_artist_search(self):
         data = self.api.artist_search(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
 
 class TestApiTrackMethods(SuperTestCase):
@@ -139,42 +139,47 @@ class TestApiTrackMethods(SuperTestCase):
 
     def test_api_track_getBuylinks(self):
         data = self.api.track_getBuylinks(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_track_getCorrection(self):
         data = self.api.track_getCorrection(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_track_getFingerprintMetadata(self):
         data = self.api.track_getFingerprintMetadata(
             fingerprintid='1234', **self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_track_getInfo(self):
         data = self.api.track_getInfo(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_track_getShouts(self):
         data = self.api.track_getShouts(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_track_getSimilar(self):
         data = self.api.track_getSimilar(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_track_getTags(self):
         data = self.api.track_getTags(user='RJ', **self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_track_getTopFans(self):
         data = self.api.track_getTopFans(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_track_getTopTags(self):
         data = self.api.track_getTopTags(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
     def test_api_track_search(self):
         data = self.api.track_search(**self.kwargs)
-        self.assertFalse(data.has_key('error'))
+        self.assertFalse('error' in data)
 
+if __name__ == "__main__":
+    # test = TestApiTrackMethods()
+    # test.setUp()
+    # test.test_api_track_search()
+    unittest.main()
